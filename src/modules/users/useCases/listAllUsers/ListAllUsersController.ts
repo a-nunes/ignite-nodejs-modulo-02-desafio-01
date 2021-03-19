db813776156e7b2b52bb984837313ee62c5c9a43
Artur Nunes <art.nunes@gmail.com>
2021-03-19 20:30:23 -0300

    Get all tets to pass.

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,18 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    if (Array.isArray(user_id)) {
+      throw new Error("Id must be a string and not an array");
+    }
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(users);
+    } catch (e) {
+      return response.status(400).json({ error: (e as Error).message });
+    }
   }
 }
 
